03e0d4717bebb683141e1be932bd36cc41d1dbc4
gigo101 <ianjay.padios@gmail.com>
2013-03-07 10:33:57 +0800

    add includes folder

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<footer>
+                        <hr>
+            
+                        <p class="pull-right">Powered by <a href="#" target="_blank">Bootstrap</a> by <a href="http://www.portnine.com" target="_blank">Portnine</a></p>
+                        
+
+                        <p>&copy; 2012 <a href="#" target="_blank">Padie</a></p>
+                    </footer>
+                    
+            </div>
+        </div>
+    </div>
+
+    <script src="lib/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript">
+        $("[rel=tooltip]").tooltip();
+        $(function() {
+            $('.demo-cancel-click').click(function(){return false;});
+        });
+    </script>
+    
+  </body>
+</html>

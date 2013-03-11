8a0b02f21b1d26e73cb191474103d160b289b460
gigo101 <ianjay.padios@gmail.com>
2013-03-11 11:41:39 +0800

    add upload_grade.php

---
+++ upload_grade.php
@@ -0,0 +1,139 @@
+
+<?php
+  require_once("includes/connection.php");
+?>
+
+
+<?php
+
+if ( isset( $_FILES['userfile'] ) )
+{
+  $csv_file = $_FILES['userfile']['tmp_name'];
+
+  if ( ! is_file( $csv_file ) )
+    exit('File not found.');
+
+  $sql = '';
+
+  if (($handle = fopen( $csv_file, "r")) !== FALSE)
+  {
+      while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
+      {
+          $sql = mysql_query("INSERT INTO `grade` SET
+            `studid` = '$data[0]',
+            `termid` = '$data[2]',
+            `grade` = '$data[3]';
+          ");
+      }
+      fclose($handle);
+  }
+
+  // Insert into database
+
+  //exit( $sql );
+  exit( "Complete!" );
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>E-Learning</title>
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="stylesheet" type="text/css" href="lib/bootstrap/css/bootstrap.css">
+    
+    <link rel="stylesheet" type="text/css" href="stylesheets/theme.css">
+    <link rel="stylesheet" href="lib/font-awesome/css/font-awesome.css">
+
+    <script src="lib/jquery-1.7.2.min.js" type="text/javascript"></script>
+
+    <!-- Demo page code -->
+
+    <style type="text/css">
+        #line-chart {
+            height:300px;
+            width:800px;
+            margin: 0px auto;
+            margin-top: 1em;
+        }
+        .brand { font-family: georgia, serif; }
+        .brand .first {
+            color: #ccc;
+            font-style: italic;
+        }
+        .brand .second {
+            color: #fff;
+            font-weight: bold;
+        }
+    </style>
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="../assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <!--[if lt IE 7 ]> <body class="ie ie6"> <![endif]-->
+  <!--[if IE 7 ]> <body class="ie ie7 "> <![endif]-->
+  <!--[if IE 8 ]> <body class="ie ie8 "> <![endif]-->
+  <!--[if IE 9 ]> <body class="ie ie9 "> <![endif]-->
+  <!--[if (gt IE 9)|!(IE)]><!--> 
+  <body class=""> 
+  <!--<![endif]-->
+    
+    <div class="navbar">
+        <div class="navbar-inner">
+                <ul class="nav pull-right">
+                    
+                </ul>
+                <a class="brand" href="index.php"><span class="first">Online E</span> <span class="second">Learning and Viewing of Grades</span></a>
+        </div>
+    </div>
+    
+
+
+    
+
+    
+        <div class="row-fluid">
+    <div class="dialog">
+        <div class="block">
+            <p class="block-heading">Upload Grades</p>
+            <div class="block-body">
+                  <form enctype="multipart/form-data" method="POST">
+                    <input class="input-file" name="userfile" type="file">
+                 <input type="submit" class="btn btn-inverse" value="Upload">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    
+
+
+    <script src="lib/bootstrap/js/bootstrap.js"></script>
+    <script type="text/javascript">
+        $("[rel=tooltip]").tooltip();
+        $(function() {
+            $('.demo-cancel-click').click(function(){return false;});
+        });
+    </script>
+    
+  </body>
+</html>
+
+
